79c59381ce783a611cae2d2df4812add82d31cb5
Marlon Boehland <kamephis@gmail.com>
2023-08-12 23:04:26 +0200

    updated post controller

--- src/Controller/PostsController.php
+++ src/Controller/PostsController.php
@@ -5,14 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\PostRepository;
 
 class PostsController extends AbstractController
 {
-    #[Route('/posts', name: 'app_posts')]
-    public function index(): Response
+    #[Route('/blog', name: 'app_posts')]
+    public function index(PostRepository $postRepository): Response
     {
+        $posts = $postRepository->findAll();
+
         return $this->render('posts/index.html.twig', [
-            'controller_name' => 'PostsController',
+            'posts' => $posts,
         ]);
     }
 }
